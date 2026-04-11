699af9a8334af1beb16967b5b61e6cef51d330e1
kachi-dev-stack <wonyekachi67@gmail.com>
2026-04-11 12:26:54 +0100

    added .env updated

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -4,13 +4,6 @@ import { getFirestore } from "firebase/firestore";
 
 // Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBrv5rsBPoMbkJhK4fX7Z3o1QWRoSUtAtY",
-  authDomain: "handyhub-16f7e.firebaseapp.com",
-  projectId: "handyhub-16f7e",
-  storageBucket: "handyhub-16f7e.appspot.com",
-  messagingSenderId: "559152605817",
-  appId: "1:559152605817:web:7bc38889a242ecb5d342a8",
-
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
   projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
