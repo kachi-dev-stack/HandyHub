c10216c3239689029564b4477380d9c756ea3228
kachi-dev-stack <wonyekachi67@gmail.com>
2026-04-11 12:22:31 +0100

    added .env

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -4,12 +4,12 @@ import { getFirestore } from "firebase/firestore";
 
 // Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBrv5rsBPoMbkJhK4fX7Z3o1QWRoSUtAtY",
-  authDomain: "handyhub-16f7e.firebaseapp.com",
-  projectId: "handyhub-16f7e",
-  storageBucket: "handyhub-16f7e.appspot.com",
-  messagingSenderId: "559152605817",
-  appId: "1:559152605817:web:7bc38889a242ecb5d342a8",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
